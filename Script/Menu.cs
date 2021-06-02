eedb69d9a3a231f4fb8115b14d2c139c6d672004
DiegoRJGuaratiba <diego72hpm@gmail.com>
2021-06-02 02:17:33 -0300

    gameCompleteV1
    sem anúncios

---
+++ Script/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class Menu : MonoBehaviour
+{
+
+
+public Button play ;
+public Button sair ;
+public Image menu;
+public Image game;
+
+
+
+ void Start(){
+
+     play.onClick.AddListener(() => { 
+
+
+     menu.gameObject.SetActive(false);
+     game.gameObject.SetActive(true);
+     });
+
+     sair.onClick.AddListener(() => { 
+         Application.Quit();
+    });
+
+ }
+
+
+}
